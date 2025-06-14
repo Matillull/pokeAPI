8c3acb2f7fb662dbbd84cdeb26cabdd2f9178e98
Macarena <maca13@live.com>
2025-06-14 11:43:31 -0300

    Agregando estilos a index y a detail. Implementando cosas extras en el archivo style

--- pokeexplorer/pokedex/views.py
+++ pokeexplorer/pokedex/views.py
@@ -3,6 +3,28 @@ from django.shortcuts import render
 
 API_BASE = "https://pokeapi.co/api/v2"
 
+# Diccionario con colores por tipo
+TYPE_COLORS = {
+    "fire": "#F08030",
+    "water": "#6890F0",
+    "grass": "#78C850",
+    "electric": "#F8D030",
+    "psychic": "#F85888",
+    "ice": "#98D8D8",
+    "dragon": "#7038F8",
+    "dark": "#705848",
+    "fairy": "#EE99AC",
+    "normal": "#A8A878",
+    "fighting": "#C03028",
+    "flying": "#A890F0",
+    "poison": "#A040A0",
+    "ground": "#E0C068",
+    "rock": "#B8A038",
+    "bug": "#A8B820",
+    "ghost": "#705898",
+    "steel": "#B8B8D0"
+}
+
 def index(request):
     return render(request, 'pokedex/index.html')
 
@@ -20,11 +42,16 @@ def pokemon_detail(request, name):
         if types:
             type_data = requests.get(f"{API_BASE}/type/{types[0]}/").json()
             weaknesses = [d['name'] for d in type_data['damage_relations']['double_damage_from']]
+            
+        # Obtener color de fondo según el primer tipo
+            main_type = types[0] if types else "normal"
+            bg_color = TYPE_COLORS.get(main_type, "#999999")
 
         return render(request, 'pokedex/detail.html', {
             'pokemon': pokemon,
             'types': types,
-            'weaknesses': weaknesses
+            'weaknesses': weaknesses,
+            'bg_color': bg_color #Nuevo fragmento vinculado a los colores
         })
     except:
         return render(request, 'pokedex/detail.html', {'error': f"No se encontró el Pokémon '{name}'."})
